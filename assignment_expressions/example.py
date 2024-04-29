5e3a8bacef93e8ef91641221d115c3c538e5ecd5
robsingh <robie10895@gmail.com>
2024-04-28 21:14:31 -0400

    more examples

--- assignment_expressions/example.py
+++ assignment_expressions/example.py
@@ -15,3 +15,13 @@ squared = [x ** 2 for x in data if x > 2]
 # with walrus operator
 data = [1,2,3,4,5]
 squared = [y ** 2 for x in data if (y := x) > 2]
+
+# Without walrus operator
+value = 10
+if value > 5 and value % 2 == 0:
+    print("Value is greater than 5 and even")
+
+# With walrus operator
+value = 10
+if (value := 10) > 5 and value % 2 == 0:
+    print("Value is greater than 5 and even")
