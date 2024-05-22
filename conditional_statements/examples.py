b697dcdd66a190593cf5fe7434cf8936f882043a
robsingh <robie10895@gmail.com>
2024-05-22 15:14:16 -0400

    conditional statements examples

---
+++ conditional_statements/examples.py
@@ -0,0 +1,19 @@
+'''
+Write a Python program that checks the temperature and provides a message based on the 
+temperature value. 
+
+If the temperature is above 27 degrees Celsius, it should print "It's a hot day." 
+If the temperature is 27 degrees or below, it should print "It's not a hot day."
+'''
+'''
+temperature = int(input("Enter the temperature: "))
+
+if temperature > 27:
+    print("it's a hot day!")
+else:
+    print("it's not a hot day")
+'''
+
+
+
+
