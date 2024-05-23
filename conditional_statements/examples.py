380e22fb27ce6d08e5f4ec120568ad37a79e3771
robsingh <robie10895@gmail.com>
2024-05-23 10:04:04 -0400

    conditional statements

--- conditional_statements/examples.py
+++ conditional_statements/examples.py
@@ -62,3 +62,17 @@ elif age >= 20 and age <= 64:
 else:
     print("Seniors")'''
 
+'''
+A leap year is a calendar year containing an additional day added to keep the calendar year synchronized with the astronomical or 
+seasonal year. 
+In the Gregorian calendar, each leap year has 366 days instead of 365, by extending February to 29 days rather than the common 28. 
+These extra days occur in years which are multiples of four (with the exception of centennial years not divisible by 400). 
+Write a Python program, which asks for a year and calculates, if this year is a leap year or not.
+'''
+'''year = int(input("Enter the year to see if it is leap year or not: "))
+
+if (year % 4 == 0 and year % 100 != 0)or (year % 400 != 0):
+    print(f"{year} is a leap year!")
+else:
+    print(f"{year} is not a leap year!")'''
+
