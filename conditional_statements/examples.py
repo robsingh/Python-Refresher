1f8bd6ba8825cd6f970f84452414eba53b979270
robsingh <robie10895@gmail.com>
2024-05-24 12:54:04 -0400

    conditional statements

--- conditional_statements/examples.py
+++ conditional_statements/examples.py
@@ -76,3 +76,76 @@ if (year % 4 == 0 and year % 100 != 0)or (year % 400 != 0):
 else:
     print(f"{year} is not a leap year!")'''
 
+'''
+Body mass index (BMI) is a value derived from the mass (weight) and height of a person. 
+The BMI is defined as the body mass divided by the square of the body height, and is universally expressed in units of kg/m2, 
+resulting from mass in kilograms and height in metres. 
+Write a program, which asks for the length and the weight of a person and returns an evaluation string. 
+'''
+
+'''height = float(input("Enter the height of the person:  "))
+weight = float(input("Enter the weight of the person: "))
+
+bmi = weight/height ** 2
+print(bmi)
+
+if bmi < 15:
+    print("Very severly underweight")
+elif bmi < 16:
+    print("Severely underweight")
+elif bmi < 18.5:
+    print("Underweight")
+elif bmi < 25:
+    print("Normal (healthy weight)")
+elif bmi < 30:
+    print("Overweight")
+elif bmi < 35:
+    print("Obese Class I (Moderately obese)")
+elif bmi < 40:
+    print("Obese Class II (Severely obese)")
+else:
+    print("Obese Class III (Very severely obese)")'''
+
+'''
+Write a Python program that calculates and displays a student's final grade based on their scores for various components of a course. 
+The program should take into account scores for homework assignments, quizzes, and a final exam. 
+It should also consider the course grading policy:
+
+Homework assignments: 20% of the final grade.
+Quizzes: 30% of the final grade.
+Final exam: 50% of the final grade.
+'''
+homework_score = float(input("Enter your homework score(out of 100): "))
+quiz_score = float(input("Enter your quiz score(out of 100): "))
+exam_score = float(input("Enter your final exam score(out of 100): "))
+
+homework_weight = 0.20
+quiz_weight = 0.30
+exam_weight = 0.50
+
+final_grade = (homework_score * homework_weight
+               + quiz_score * quiz_weight
+               + exam_score * exam_weight)
+
+if final_grade >= 90:
+    letter_grade = 'A'
+elif final_grade >= 80:
+    letter_grade = 'B'
+elif final_grade >= 70:
+    letter_grade = 'C'
+elif final_grade >= 60:
+    letter_grade = 'D'
+else:
+    letter_grade = 'F'
+
+print("Your final grade is: ", final_grade)
+print("Your letter grade is: ", letter_grade)
+
+
+
+
+
+
+
+
+
