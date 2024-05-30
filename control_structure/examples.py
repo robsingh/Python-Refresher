5f4173bddbae3a8c77ffc68591937d410e96a7e8
robsingh <robie10895@gmail.com>
2024-05-30 11:55:22 -0400

    more clean code

--- control_structure/examples.py
+++ control_structure/examples.py
@@ -5,22 +5,23 @@ The program informs the player, if this number is larger, smaller or equal to th
 i.e. the number which the program has randomly created. 
 If the player wants to gives up, he or she can input a 0 or a negative number.
 '''
-
 import random
 upper_bound = 20
 lower_bound = 1
-
 to_be_guessed = random.randint(lower_bound, upper_bound)
 guess = 0
 while guess != to_be_guessed:
     guess = int(input("New number: "))
-    if guess > 0:
-        if guess > to_be_guessed:
-            print("Number too large!")
-        elif guess < to_be_guessed:
-            print("Number too small!")
-    else:
+    if guess == 0:
         print("Sorry that you are giving up!")
-        break #break out of the loop, don't execute else
+        break
+    if guess < lower_bound or guess > to_be_guessed:
+            print("Guess not within the boundaries!")
+    elif guess > to_be_guessed:
+        upper_bound = guess - 1
+        print("Number too large!")
+    elif guess < to_be_guessed:
+         lower_bound = guess + 1
+         print("Number too small!")
 else:
     print("Congratulations! You did it.")
