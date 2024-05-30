216142073519af57b09dbe2d648d0de150faf631
robsingh <robie10895@gmail.com>
2024-05-30 12:24:45 -0400

    example

--- control_structure/examples.py
+++ control_structure/examples.py
@@ -5,7 +5,7 @@ The program informs the player, if this number is larger, smaller or equal to th
 i.e. the number which the program has randomly created. 
 If the player wants to gives up, he or she can input a 0 or a negative number.
 '''
-import random
+'''import random
 upper_bound = 20
 lower_bound = 1
 to_be_guessed = random.randint(lower_bound, upper_bound)
@@ -25,3 +25,21 @@ while guess != to_be_guessed:
          print("Number too small!")
 else:
     print("Congratulations! You did it.")
+'''
+
+#dog to human age convertor -> 0 or a negative value means that they want to finish.
+dog_age = 1
+while dog_age > 0:
+    dog_age = int(input("Age of the dog: (0 or negative number to finish)"))
+    print()
+    if dog_age <= 0:
+        human_age = -1
+    elif dog_age == 1:
+        human_age = 14
+    elif dog_age >= 2:
+        human_age = 22 + (dog_age - 2) * 5
+
+    print("Corresponds to " + str(human_age) + " human years!")
+
+print("Program finished!")
+
